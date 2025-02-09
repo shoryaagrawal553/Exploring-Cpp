3b59931bb82ab6d32267b8e100a7d4e02291b20a
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-09 17:24:42 +0530

    Add files via upload

---
+++ prime no. improved(using flag).cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, flag=0;
+    cout<<"input ";
+    cin>>n;
+    if(n>1)
+    {
+        for(i=2; i<n; i++)
+        {
+            if(n%i==0)
+            {
+                cout<<n<<" is not prime";
+                flag++;
+                break;
+            }
+    // else 
+    // {
+    //     cout<<n<<" is prime";     
+    //     break;                    
+    // }                             
+        }
+        if (flag==0)
+        {
+            cout<<n<<" is prime ";
+        }
+    }
+    // else 
+    // {
+    //     cout<<"invalid input";
+    // }
+
+}
