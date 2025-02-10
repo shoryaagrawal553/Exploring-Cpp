9a4c6c4a6cbe70cdc62ac7df12c89f3ade34e853
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-10 18:07:37 +0530

    Add files via upload

---
+++ ternary statement.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //condition? stt1: stt2;
+    //stt1= if condition true, stt2= else or if condition false
+
+    int n=54;
+    cout<<(n>0? "positive": "negetive")<<endl;
+    return 0;
+}
