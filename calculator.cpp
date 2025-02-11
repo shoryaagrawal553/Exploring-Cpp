38c9c5fef260ae44810619247ff999e698bbbcb1
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-11 11:22:53 +0530

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    float a, b;
-    char s;
-    cout<<"enter your input";
-    cin>>a>>s>>b;
-    switch (s)
+    float a, b;  // Declare variables to store two numbers
+    char s;  // Declare variable to store the operator
+
+    cout << "Enter your input: ";  // Prompt user for input
+    cin >> a >> s >> b;  // Take input in the format: number operator number
+
+    switch (s)  // Switch case to check the operator
     {
-        case '+':
-        cout<<"= "<<a+b;
-        break;
-        case '-':
-        cout<<"= "<<a-b;
-        break;
-        case '*':
-        cout<<"= "<<a*b;
-        break;
-        case '/':
-        if(b!=0){
-            cout<<"= "<<a/b;
-        }
-        else{
-            cout<<"infinity ";
-        }
-        break;
-        default:
-        cout<<"wrong input";
-        break;
+        case '+':  // If operator is '+'
+            cout << "= " << a + b;  // Perform addition
+            break;
+        case '-':  // If operator is '-'
+            cout << "= " << a - b;  // Perform subtraction
+            break;
+        case '*':  // If operator is '*'
+            cout << "= " << a * b;  // Perform multiplication
+            break;
+        case '/':  // If operator is '/'
+            if (b != 0) {  // Check if divisor is not zero
+                cout << "= " << a / b;  // Perform division
+            } else {
+                cout << "infinity ";  // Print infinity if divisor is zero
+            }
+            break;
+        default:  // If operator is not recognized
+            cout << "wrong input";  // Print error message
+            break;
     }
-    return 0;
+
+    return 0;  // Indicate successful program termination
 }
