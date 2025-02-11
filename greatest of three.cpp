a1a0e54c15355562d7c136aad8a8bd49e7476b79
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-11 11:24:23 +0530

    Update greatest of three.cpp

--- greatest of three.cpp
+++ greatest of three.cpp
@@ -1,19 +1,30 @@
 #include<iostream>
 using namespace std;
+
 int main()
 {
-  int a, b, c;
-  cout<<"Enter three numbers ";
-  cin>>a>>b>>c;
-  if(a>b && a>c)
-  {
-    cout<<" A is greatestt";
-  }
-  else{
-    if(b>c)
-    { cout<<"B is greatest ";}
-    else{
-      cout<<"C is greatest";
+    int a, b, c;  // Declare three integer variables
+
+    cout << "Enter three numbers: ";  // Prompt user to enter three numbers
+    cin >> a >> b >> c;  // Read the three numbers from user input
+
+    // Check if 'a' is greater than both 'b' and 'c'
+    if (a > b && a > c)
+    {
+        cout << "A is greatest";  // If true, print that 'A' is the greatest
     }
-  }
+    else
+    {
+        // If 'a' is not the greatest, check if 'b' is greater than 'c'
+        if (b > c)
+        {
+            cout << "B is greatest";  // If true, print that 'B' is the greatest
+        }
+        else
+        {
+            cout << "C is greatest";  // If 'b' is not greater than 'c', print that 'C' is the greatest
+        }
+    }
+
+    return 0;  // Indicate successful program termination
 }
