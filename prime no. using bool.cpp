39b8864a2883aaf922ec677dd615021e3f79d2fd
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-12 11:12:07 +0530

    Update prime no. using bool.cpp

--- prime no. using bool.cpp
+++ prime no. using bool.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int n, i;
     cout << "enter ";
-    cin >> n;
-    bool isprime=true;
+    cin >> n;  // Input the number to check for prime
+
+    bool isprime = true;  // Initialize a boolean variable to true
+
+    // Loop from 2 to n-1 to check for factors
     for (i = 2; i < n; i++)
     {
+        // If n is divisible by i, it's not a prime number
         if (n % i == 0)
         {
-            cout<< n << " is not prime";
-            isprime = false;
-            break;
+            cout << n << " is not prime";  // Output that n is not prime
+            isprime = false;  // Set the boolean variable to false
+            break;  // Exit the loop since we found a factor
         }
     }
 
+    // If the boolean variable is still true, n is a prime number
     if(isprime == true)
     {
-        cout<< n << " is prime";
+        cout << n << " is prime";  // Output that n is prime
     }
 
-    return 0;
+    return 0;  // Return 0 to indicate successful execution
 }
