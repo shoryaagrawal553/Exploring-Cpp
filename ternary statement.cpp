79a1707263d66eb42d3ee8fc2da425afe38b6127
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-13 21:21:23 +0530

    Update ternary statement.cpp

--- ternary statement.cpp
+++ ternary statement.cpp
@@ -1,11 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main()
 {
-    //condition? stt1: stt2;
-    //stt1= if condition true, stt2= else or if condition false
+    // condition ? stt1 : stt2;
+    // stt1 = if condition is true, stt2 = else or if condition is false
 
-    int n=54;
-    cout<<(n>0? "positive": "negetive")<<endl;
-    return 0;
+    int n = 54; // Initialize variable n with value 54
+    // Using ternary operator to check if n is positive or negative
+    cout << (n > 0 ? "positive" : "negative") << endl;
+    return 0; // Return 0 to indicate successful execution
 }
