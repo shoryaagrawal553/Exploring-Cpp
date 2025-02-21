20f4bbcea5040d9ac7b78396d83903c030307e4d
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-21 22:45:49 +0530

    Update no_semicollons.cpp

--- no_semicollons.cpp
+++ no_semicollons.cpp
@@ -1,10 +1,13 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Include the input-output stream library
+using namespace std; // Use the standard namespace
+
 int main()
 {
+    // Print "Helloooo!!" using the cout object inside an if statement
     if(cout<<"Helloooo!!\n"){}
 
+    // Print "Helloooo!!!" using the cout object inside a switch statement
     switch (cout<<"Helloooo!!!", 0){}
 
-    return 0;
+    return 0; // Return 0 to indicate successful execution
 }
