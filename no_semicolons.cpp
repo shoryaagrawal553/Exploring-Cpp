452572f0e1047854276df930cea7ad2ea03bc6ad
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-02-23 13:42:47 +0530

    comments edited

--- no_semicolons.cpp
+++ no_semicolons.cpp
@@ -9,4 +9,9 @@ int main()
     switch (cout<<"Helloooo!!!", 0){}
 
     return 0; // Return 0 to indicate successful execution
+    // return statement can also be ommitted because it 
+    // is the last statement in the main function 
+    // and the compiler will automatically insert it
+    // if it is not present because the main function 
+    // is of type int
 }
