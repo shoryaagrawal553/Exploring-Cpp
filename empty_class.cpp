1d9c7528af9f5fba269b8e6f2aa771ed30cf4136
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-03-06 16:42:29 +0530

    Add files via upload

---
+++ empty_class.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class EmptyClass {
+    // Empty class
+};
+
+int main() {
+    EmptyClass obj;
+    cout << "An object of EmptyClass has been created." << endl;
+    return 0;
+}
