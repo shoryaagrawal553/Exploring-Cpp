d853c8a18bea45847437eb0c5e178ba819305800
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-03-06 16:41:59 +0530

    Add files via upload

---
+++ file_I_O.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    //cout << "to write in a file";
+
+    ofstream outFile("sample_file.txt");
+    // OR ofstream out ("sample_file.txt");
+
+    outFile << "this is a sample file \nThis is the second line of the file";
+
+    outFile.close();
+
+    cout<<"to read from the file \n";
+
+    ifstream inFile("sample_file.txt");
+    string data;;
+    while (getline(inFile, data))
+    {
+        cout<<data<<endl;
+    }
+
+    return 0;
+}
