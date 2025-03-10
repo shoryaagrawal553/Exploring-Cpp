f2931d6e8bda31734c0e12ea12dfeec0c1976024
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-03-10 22:24:16 +0530

    Add files via upload

---
+++ box pattern.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+
+    // cout<<"for printing 5 stars in n lines/rows\n";
+    // int n= 3;
+    // for(i=1; i<=n; i++)  //for printing 5 stars in n lines/rows
+    // {
+    //     cout<<"*****";
+    //     cout<<endl;  //to get to next line
+    // }
+
+    // cout<<"for printing n stars per line\n";
+    // n=5;
+    // for(i=1; i<=n; i++) //for printing n stars per line
+    // {
+    //     cout<<"* ";
+    // }
+
+    // cout<<"\nfor printing n stars in n lines\n";
+    int n=3;
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n; j++)
+        {
+            cout<<"* ";    
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
