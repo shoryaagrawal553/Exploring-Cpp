5a0ea5dfe5e0b7fcef22389faf9b25353a8c6a59
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-03-13 21:57:19 +0530

    Update sq pat1.cpp

--- sq pat1.cpp
+++ sq pat1.cpp
@@ -3,22 +3,27 @@
     1 2 3
     1 2 3 */
 
-
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int n, i, j;
 
+    // Prompt the user to enter the value of n
     cout << "enter ";
-    cin >> n;
+    cin >> n; // Read the value of n from user input
 
+    // Loop through rows
     for (i = 1; i <= n; i++)
     {
+        // Loop through columns
         for (j = 1; j <= n; j++)
         {
+            // Print the value of j
             cout << j << " ";
         }
+        // Move to the next line after each row
         cout << endl;
     }
 
