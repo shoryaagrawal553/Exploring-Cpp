215f7e8088e6779cf991acb6d1e69101351c33a5
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-03-14 22:13:37 +0530

    Add files via upload

---
+++ tri pat1.cpp
@@ -0,0 +1,22 @@
+/*enter 3
+* 
+* *
+* * *   */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, n;
+    cout<<"enter ";
+    cin>>n;
+
+    for(i= 1; i<=n; i++)
+    {
+        for(j=1; j<=i; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
