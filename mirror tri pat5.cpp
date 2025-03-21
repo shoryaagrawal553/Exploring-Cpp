a96420ed2cc0e46ef9ce704b9e4e8cb0ccb4d7f1
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-03-21 22:42:53 +0530

    Add files via upload

---
+++ mirror tri pat5.cpp
@@ -0,0 +1,31 @@
+/*enter 4
+      1 
+    2 1 
+  3 2 1 
+4 3 2 1   */
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i, j;
+    cout<<"enter ";
+    cin>>n;
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=1; j<=n-i; j++)
+        {
+            cout<<"  ";
+        }
+
+        for(j=i; j>=1; j--)
+        {
+            cout<<j<<" ";
+        }
+
+        cout<<endl;
+    }
+
+    return 0;
+}
