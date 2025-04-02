68d01d56b5bae1cc433850038c3b99def24a17ee
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-02 23:41:30 +0530

    Update hollow_dia_pat.cpp

--- hollow_dia_pat.cpp
+++ hollow_dia_pat.cpp
@@ -1,61 +1,77 @@
-/*enter 4
+/*
+enter 4
       *       
     *   *     
   *       *   
 *           * 
   *       *   
     *   *     
-      *     */
+      *     
+*/
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    // Declare variables
     int i, j, n;
-    cout<<"enter ";
-    cin>>n;
+    
+    // Prompt user to enter the size of the pattern
+    cout << "Enter the size of the pattern: ";
+    cin >> n;
 
-    for(i=0; i<n; i++)
+    // Loop to print the upper half of the hollow diamond pattern
+    for(i = 0; i < n; i++)
     {
-        for(j=1; j<= n-i-1; j++) // spaces
+        // Print leading spaces
+        for(j = 1; j <= n - i - 1; j++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        cout<<"* ";
-
-        for(j=1; j<=i*2-1; j++) // inner spaces
+        cout << "* ";
+        
+        // Print inner spaces
+        for(j = 1; j <= i * 2 - 1; j++)
         {
-            cout<<"  ";
-        }
-
-        if(i!=0)
-        {
-            cout<<"* ";
+            cout << "  ";
         }
 
-        cout<<endl;
+        // Print the second star for all rows except the first
+        if(i != 0)
+        {
+            cout << "* ";
+        }
+
+        // Move to the next line
+        cout << endl;
     }
 
-    for(i=n-2; i>=0; i--)
+    // Loop to print the lower half of the hollow diamond pattern
+    for(i = n - 2; i >= 0; i--)
     {
-        for(j=1; j<= n-i-1; j++) // spaces
+        // Print leading spaces
+        for(j = 1; j <= n - i - 1; j++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        cout<<"* ";
+        cout << "* ";
 
-        for(j=1; j<=i*2-1; j++) // inner spaces
+        // Print inner spaces
+        for(j = 1; j <= i * 2 - 1; j++)
         {
-            cout<<"  ";
-        }
-
-        if(i!=0)
-        {
-            cout<<"* ";
+            cout << "  ";
         }
 
-        cout<<endl;
+        // Print the second star for all rows except the last
+        if(i != 0)
+        {
+            cout << "* ";
+        }
+
+        // Move to the next line
+        cout << endl;
     }
+    
     return 0;
 }
