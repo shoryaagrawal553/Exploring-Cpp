ac23e3fd0451fb2472e2a6aba73063676fe456c2
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-02 23:40:03 +0530

    Update dec_to_bin.cpp

--- dec_to_bin.cpp
+++ dec_to_bin.cpp
@@ -3,23 +3,33 @@ using namespace std;
 
 int main()
 {
-    int n, rem, bino=0, pow=1;
-    cout<<"Enter the decimal number ";
-    cin>>n;
-    int orig=n;
-    while (n>0)
+    // Declare variables
+    int n, rem, bino = 0, pow = 1;
+    
+    // Prompt user to enter a decimal number
+    cout << "Enter the decimal number: ";
+    cin >> n;
+    
+    // Store the original number for output purposes
+    int orig = n;
+    
+    // Loop to convert decimal to binary
+    while (n > 0)
     {
-        rem=n%2;
-        n=n/2;
-        bino=(rem*pow)+bino;
-        pow*=10;
+        rem = n % 2;    // Get the remainder when n is divided by 2
+        n = n / 2;      // Update n to be n divided by 2
+        bino = (rem * pow) + bino;  // Update the binary number
+        pow *= 10;      // Update the power of 10
     }
 
-    cout<<"binary of "<<orig<<" is "<<bino;
+    // Output the binary representation of the original number
+    cout << "Binary of " << orig << " is " << bino;
 
     return 0;
 }
-/*Iteration 1:
+
+/*
+Iteration 1:
 rem = n % 2 = 3 % 2 = 1
 n = n / 2 = 3 / 2 = 1
 bino = (rem * pow) + bino = (1 * 1) + 0 = 1
@@ -42,10 +52,10 @@ n = 0
 rem = 1
 bino = 11
 pow = 100
+
 Loop Ends:
 The loop ends because n = 0.
 
 Final Output:
 The binary representation of 3 is 11. The program prints:
-
 */
