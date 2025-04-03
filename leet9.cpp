ff66a74e4220c53151c310501c63250fe307449b
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-03 21:17:24 +0530

    Update leet9.cpp

--- leet9.cpp
+++ leet9.cpp
@@ -1,39 +1,30 @@
 // palindrome number
 // A palindrome number is a number that remains the same when its digits are reversed. For example, 121, 12321, and 45654 are palindrome numbers.
-// a -ve no. can't be a palindrome no. -121 == 121- (when rervresed)
+// a -ve no. can't be a palindrome no. -121 == 121- (when reversed)
+
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int n, rem, rev=0, orig;
-    
-    cout<<"enter a number ";
-    cin>>n;
-    orig=n;
+    int n, rem, rev = 0, orig;
 
-    if(n<0)
+    // Prompt the user to enter a number
+    cout << "enter a number ";
+    cin >> n;
+
+    // Store the original number
+    orig = n;
+
+    // Check if the number is negative
+    if (n < 0)
     {
-        cout<<orig<<" is not a palindrome";
-        return 0;
-    }
-    while(n!=0)
-    {
-        rem=n%10;
-        n=n/10;
-        rev=(rev*10)+rem;
-    }
-    if(orig==rev)
-    {
-        cout<<orig<<" is a palindrome";
+        // If the number is negative, it cannot be a palindrome
+        cout << orig << " is not a palindrome";
         return 0;
     }
 
-    cout<<orig<<" is not a palindrome";
-    return 0;
-}
-
-    /* if(ans>INT_MAX/10) // check for overflow
-            {
-                return 0;
-            }*/
+    // Reverse the digits of the number
+    while (n != 0)
+    {
+        rem = n %  ▋
