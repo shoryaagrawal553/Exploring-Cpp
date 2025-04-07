5cd171d3bce9f52b1d6d65328d5f890f2c72ec06
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-07 23:52:09 +0530

    Update arr_rotate_by_1.cpp

--- arr_rotate_by_1.cpp
+++ arr_rotate_by_1.cpp
@@ -3,27 +3,32 @@ using namespace std;
 
 int main()
 {
-    int n= 6, i;
-    int arr[n]={1,2,3,4,5,6};
-    int temp = arr[n-1];
+    int n = 6, i; // Initialize the size of the array and the loop variable
+    int arr[n] = {1, 2, 3, 4, 5, 6}; // Initialize the array with 6 elements
+    int temp = arr[n-1]; // Store the last element of the array in a temporary variable
 
-    cout<<"initial array \n";
-    for(i=0; i<n; i++)
+    // Print the initial array
+    cout << "Initial array \n";
+    for (i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
+    cout << endl;
 
-    for( i=n-2; i>=0; i--)
+    // Rotate the array elements to the right by one position
+    for (i = n - 2; i >= 0; i--)
     {
-        arr[i+1]= arr[i];
+        arr[i + 1] = arr[i]; // Shift each element to the right
     }
-    arr[0]=temp;
+    arr[0] = temp; // Place the last element at the first position
 
-    cout<<"\nReversed array \n";
-    for(i=0; i<n; i++)
+    // Print the rotated array
+    cout << "\nRotated array \n";
+    for (i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
