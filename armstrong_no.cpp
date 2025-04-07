2b812c05948404885ce287d4b6c98b6a6fec9d92
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-07 23:49:23 +0530

    Update armstrong_no.cpp

--- armstrong_no.cpp
+++ armstrong_no.cpp
@@ -6,65 +6,50 @@
 #include<cmath>
 using namespace std;
 
-int count_digit (int num)  // or int countdigit (int n)
-{// to count the number of digits
-
-    int count;
-    while(num!=0)
+// Function to count the number of digits in a number
+int count_digit(int num) 
+{
+    int count = 0; // Variable to store the count of digits
+    while(num != 0)
     {
-        num= num/10;
-        count++;
+        num = num / 10; // Remove the last digit from the number
+        count++; // Increment the count of digits
     }
-    return count;
+    return count; // Return the total count of digits
 }
 
-int check_as(int num)  // to calculate armstrong number 
+// Function to check if a number is an Armstrong number
+int check_as(int num) 
 {
-    int count= count_digit(num);
-    int ans=0;
-    while(num!=0)
+    int count = count_digit(num); // Get the number of digits
+    int ans = 0; // Variable to store the result of the Armstrong number calculation
+    int original_num = num; // Store the original number
+    while(num != 0)
     {
-        int rem = num%10;
-        num= num/10;
-        ans= (pow(rem, count))+ans;
+        int rem = num % 10; // Get the last digit of the number
+        num = num / 10; // Remove the last digit from the number
+        ans = (pow(rem, count)) + ans; // Add the digit raised to the power of count to the result
     }
-
-    return ans;
+    return ans; // Return the calculated Armstrong number
 }
 
 int main()
 {
-    int num; // temp, orig, count=0, ans=0;
-    cout<<"enter ";
-    cin>>num;
+    int num; // Variable to store the input number
+    cout << "Enter a number: ";
+    cin >> num; // Read the input number
 
+    int ans = check_as(num); // Calculate the Armstrong number
 
-    // orig=num;
-    // temp=num;
-
-    // while(num!=0) // to count the number of digits
-    // {
-    //     num= num/10;
-    //     count++;
-    // }
-
-    // while(orig!=0)  // to calculate armstrong number 
-    // {
-    //     int rem = orig%10;
-    //     orig= orig/10;
-    //     ans= (pow(rem, count))+ans;
-    // }
-
-    int ans= check_as(num);
-
+    // Check if the calculated result is equal to the input number
     if(ans == num)
     {
-        cout<<num<<" is an armstrong number ";
+        cout << num << " is an Armstrong number ";
     }
     else
     {
-    cout<<num<<" is not an armstrong number ";
+        cout << num << " is not an Armstrong number ";
     }
 
-    return 0;
+    return 0; // Return 0 to indicate successful execution
 }
