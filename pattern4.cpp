40908f3cae72d6477278b12275cacd8f6e37d970
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-11 21:09:14 +0530

    Update pattern4.cpp

--- pattern4.cpp
+++ pattern4.cpp
@@ -1,55 +1,64 @@
-/*enter 4
-      *         
-    *   *       
-  *   *   *     
-*   *   *   *   
-*   *   *   *   
-  *   *   *     
-    *   *       
-      *         */
+/* 
+ * Program to print a diamond-shaped pattern based on user input.
+ * Example for input '4':
+ *       *         
+ *     *   *       
+ *   *   *   *     
+ * *   *   *   *   
+ * *   *   *   *   
+ *   *   *   *     
+ *     *   *       
+ *       *         
+ */
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int n,i,j;
-    cout<<"enter "; 
-    cin>>n;
+    int n, i, j;
+    cout << "Enter the number of rows for the upper half of the diamond: "; 
+    cin >> n;
 
-    for(i=1; i<=n; i++)
+    // Loop to print the upper half of the diamond
+    for (i = 1; i <= n; i++)
     {
-        for(j=i; j<n; j++) //spaces
-        { //i=1 => j=1 to 3 => 3 spaces; 
-        //i=2 => j=2 to 3 => 2 spaces; i=3 => j=3 => 1 space
-            cout<<"  "; // (a)1 character (space); (b)2 characters (spaces)
+        // Print spaces before the stars
+        for (j = i; j < n; j++) 
+        {
+            // For each row, spaces decrease as rows go down
+            cout << "  "; 
         }
 
-        for(j=1; j<=i; j++)
-        {//i=1 => j=1 => 1 star; i=2 => j=1 to 2 => 2 stars; i=3 => j=1 to 3 => 3 stars
-            cout<<"*   "; //(a)=1 star and 1space; (b) =1 star with 1 space 
-        }                                     // and 2 spaces for next space from above loop
+        // Print stars with spacing
+        for (j = 1; j <= i; j++) 
+        {
+            // Number of stars increases as rows go down
+            cout << "*   "; 
+        }
 
-        cout<<endl;
+        cout << endl; // Move to the next line after each row
     }
 
-    for(i=n; i>=1; i--)
+    // Loop to print the lower half of the diamond
+    for (i = n; i >= 1; i--)
     {
-        for(j=i; j<n; j++) //spaces
-        { //i=4 => j=4 => 0 spaces; i=3 => j=3 to 4 => 1 space; i=2 => j=2 to 4 => 2 spaces
-        //i=1 => j=1 to 4 => 3 spaces
-            cout<<"  "; 
+        // Print spaces before the stars
+        for (j = i; j < n; j++) 
+        {
+            // For each row, spaces increase as rows go up
+            cout << "  "; 
         }
 
-        for(j=1; j<=i; j++) // stars
-        { //i=4 => j=1 to 4 => 4 stars; i=3 => j=1 to 3 => 3 stars; i=2 => j=1 to 2 => 2 stars
-        //i=1 => j=1 => 1 star
-            cout<<"*   "; 
+        // Print stars with spacing
+        for (j = 1; j <= i; j++) 
+        {
+            // Number of stars decreases as rows go up
+            cout << "*   "; 
         }
 
-        cout<<endl;
+        cout << endl; // Move to the next line after each row
     }
 
-
-    return 0;
+    return 0; // Indicate successful program termination
 }
