27342ed740297eb23b49fdf4ef34c1a26d8a27fc
Shorya Agrawal <165979200+shoryaagrawal553@users.noreply.github.com>
2025-04-11 21:10:09 +0530

    Update pattern5.cpp

--- pattern5.cpp
+++ pattern5.cpp
@@ -1,54 +1,60 @@
-/* enter 4
-      *   
-    *   *
-  *   *   *
-*   *   *   *
-  *   *   *
-    *   *
-      *    */
+/* Pattern generation example:
+   For input 4:
+       *   
+     *   *
+   *   *   *
+ *   *   *   *
+   *   *   *
+     *   *
+       *   
+*/
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int n,i,j;
-    cout<<"enter "; 
-    cin>>n;
+    int n, i, j;
+    cout << "enter "; 
+    cin >> n;
 
-    for(i=1; i<n; i++)
+    // Loop to print the top half of the pattern (pyramid)
+    for (i = 1; i < n; i++) // Runs from 1 to n-1
     {
-        for(j=i; j<n; j++) //spaces
-        { //i=1 => j=1 to 3 => 3 spaces; 
-        //i=2 => j=2 to 3 => 2 spaces; i=3 => j=3 => 1 space
-            cout<<"  "; // (a)1 character (space); (b)2 characters (spaces)
+        // Print spaces (decreasing)
+        for (j = i; j < n; j++) // Spaces decrease as i increases
+        { 
+            cout << "  "; // Prints two spaces per iteration
         }
 
-        for(j=1; j<=i; j++)
-        {//i=1 => j=1 => 1 star; i=2 => j=1 to 2 => 2 stars; i=3 => j=1 to 3 => 3 stars
-            cout<<"*   "; //(a)=1 star and 1space; (b) =1 star with 1 space 
-        }                                     // and 2 spaces for next space from above loop
+        // Print stars (increasing)
+        for (j = 1; j <= i; j++) // Stars increase as i increases
+        {
+            cout << "*   "; // Prints a star followed by 3 spaces
+        }
 
-        cout<<endl;
+        // Move to the next line after each row
+        cout << endl;
     }
 
-    for(i=n; i>=1; i--)
+    // Loop to print the bottom half of the pattern (inverted pyramid)
+    for (i = n; i >= 1; i--) // Runs from n to 1
     {
-        for(j=i; j<n; j++) //spaces
-        { //i=4 => j=4 => 0 spaces; i=3 => j=3 to 4 => 1 space; i=2 => j=2 to 4 => 2 spaces
-        //i=1 => j=1 to 4 => 3 spaces
-            cout<<"  "; 
+        // Print spaces (increasing)
+        for (j = i; j < n; j++) // Spaces increase as i decreases
+        { 
+            cout << "  "; // Prints two spaces per iteration
         }
 
-        for(j=1; j<=i; j++) // stars
-        { //i=4 => j=1 to 4 => 4 stars; i=3 => j=1 to 3 => 3 stars; i=2 => j=1 to 2 => 2 stars
-        //i=1 => j=1 => 1 star
-            cout<<"*   "; 
+        // Print stars (decreasing)
+        for (j = 1; j <= i; j++) // Stars decrease as i decreases
+        {
+            cout << "*   "; // Prints a star followed by 3 spaces
         }
 
-        cout<<endl;
+        // Move to the next line after each row
+        cout << endl;
     }
-
 
     return 0;
 }
